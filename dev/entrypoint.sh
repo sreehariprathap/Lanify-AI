0da20531e8c8d96665da23cafebb19f6c84b66e9
Brendan Lee <lizheminglzm@gmail.com>
2025-04-15 18:13:50 -0400

    mlflow

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Start nginx
 service nginx start || echo "Failed to start nginx"
 
+# Start MLflow UI in the background
+mlflow ui --backend-store-uri /app/mlruns --host 0.0.0.0 --port 5000 &
+
 # Change to application directory
 cd /app/dev
 
